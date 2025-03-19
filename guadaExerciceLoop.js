c0b3f2a151d7ce6f0bcd1364bef118577669879b
Guadallupi Bezerra <guadallupibezerra@gmail.com>
2025-03-19 20:34:50 -0300

    Update and rename guadaExerciceLoopFor to guadaExerciceLoop.js

---
+++ guadaExerciceLoop.js
@@ -0,0 +1,29 @@
+//Loop FOR
+
+for (let dia = 1; dia <= 30; dia ++) [
+  console.log("Dia", dia)
+[
+
+//Dia 1
+  Dia 2
+  Dia 3
+   ...
+  Dia 30 //
+
+//Loop WHILE
+
+let dia = 1
+while(dia <= 30) {
+  console.log("Dia", dia)
+  dia++
+  {
+
+// Loop DO WHILE
+
+let dia = 1
+do {
+   console.log("Dia", dia)
+   dia++
+  { while(dia <= 30)
+
+
