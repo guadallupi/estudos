46a21876f345e48b37136559443b45115cbfc1b9
Guadallupi Bezerra <guadallupibezerra@gmail.com>
2025-03-19 13:25:49 -0300

    Create guadaExerciceArrayMethods.js

---
+++ guadaExerciceArrayMethods.js
@@ -0,0 +1,17 @@
+const listaDeSorteados = ["Ana", "Maria", "Pedro", "Paul"]
+
+listaDeSorteados.pop() //Pop: remove o último item do array
+
+listaDeSorteados.push() //Push: adiciona item no final do array
+
+listaDeSorteados.unshift() //Unshift: adiciona item como Primeiro do array
+
+listaDeSorteados.shift() //Shift: remove o Primeiro item da lista do array
+
+console.log(listaDeSorteados)
+
+//Includes: Verifica se alguém está na lista
+
+console.log("Pedro está na lista? ", listaDeSorteados.includes("Pedro") //true
+
+console.log("Júlia está na lista? ", listaDeSorteados.includes("Júlia") //false
