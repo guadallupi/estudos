c335357b33e9edc251d9ca90e43dff5ddc4633e1
Guadallupi Bezerra <guadallupibezerra@gmail.com>
2025-03-20 13:30:46 -0300

    Create GuadaExerciceMetodoForEach.js
    É o método mais usado na linguagem JavaScript

---
+++ GuadaExerciceMetodoForEach.js
@@ -0,0 +1,18 @@
+const notasFinais = [            //Uso do Array
+  {nome: "Ana", nota: 4}
+  {nome: "Murilo", nota:8}
+  {nome: "Alice", nota: 10}
+  {nome: "Julio", nota: 5}
+]
+
+notasFinais.forEach((aluno) => {    // Uso do forEach
+  if (aluno.nota >= 6) {
+  console.log("O aluno ${aluno.nome} está aprovado com ${aluno.nota}") 
+  } else {
+    console.log("O aluno ${aluno.nome} está reprovado com ${aluno.nota}")
+    }
+
+//Imprime: O aluno Ana está reprovado com 4
+           O aluno Murilo está aprovado com 8
+           O aluno Alice está aprovado com 10
+           O aluno Julio está reprovado com 5
